cfd04ece76859421b392c0e132812ce3437f4010
Cody Mailey <cmailey03@gmail.com>
2019-10-31 10:24:34 -0600

    finished money

--- money_game.rb
+++ money_game.rb
@@ -24,21 +24,45 @@ class Player
     end
 
     def draw
-        @image.draw(@x, @y, 1)
+        @image.draw(@x, @y, ZOrder::PLAYER)
+    end
+
+
+    def score
+        @score
+    end
+
+    def collect_moneys(moneys)
+        moneys.reject! do |money| 
+            if Gosu.distance(@x, @y, money.x + 10, money.y + 10) < 65 
+                @score = @score + 10
+                true
+            elsif money.y > 700
+                true
+            else
+                false
+            end
+        end
     end
 end
 
 class Money
-    attr_raeder :x, :y
+    attr_reader :x, :y
     def initialize
         @image =Gosu::Image.new("media/money.png")
-        @x = rand * 1000
-        @y = 900
+        @x = rand * 950
+        @y = -100
     end 
 
+    def fall
+        @y += 3
+    end
+
     def draw
-
-
+        @image.draw(@x,@y, ZOrder::MONEY)
+        fall
+    end 
+end
 class Money_Grab < Gosu::Window
 
     def initialize
@@ -48,6 +72,8 @@ class Money_Grab < Gosu::Window
         @background_image = Gosu::Image.new("media/background.png")
         @player = Player.new
         @player.warp(500, 450)
+        @font = Gosu::Font.new(20)
+        @moneys = Array.new
     end
     def update
         if Gosu.button_down? Gosu::KB_LEFT or Gosu::button_down? Gosu::GP_LEFT
@@ -56,12 +82,19 @@ class Money_Grab < Gosu::Window
         if Gosu.button_down? Gosu::KB_RIGHT or Gosu::button_down? Gosu::GP_RIGHT
             @player.move_right
         end
+        if rand(100) < 2 and @moneys.size < 5
+            @moneys.push(Money.new)
+        end
 
+        @player.collect_moneys(@moneys)
+ 
     end
 
     def draw
         @background_image.draw(0,0,0)
         @player.draw
+        @moneys.each { |money| money.draw}
+        @font.draw_text("Player 1 Score: #{@player.score}", 10, 10, ZOrder::UI, 1.5, 1.5, Gosu::Color::YELLOW)
 
     end
 
